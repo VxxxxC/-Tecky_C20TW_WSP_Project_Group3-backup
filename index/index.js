69ffebde6f411f5180da81d1c5bd9e01221a6e7e
Veper <veper.chesley@protonmail.com>
2022-04-30 21:30:06 +0800

    adding view and like icon into the image content box when hover

---
+++ index/index.js
@@ -0,0 +1,11 @@
+document.querySelector(".post").addEventListener("click", () => {
+  console.log(`刊登文章 clicked!`);
+});
+
+document.querySelector(".reset-password").addEventListener("click", () => {
+  console.log(`Reset password clicked`);
+});
+
+document.querySelector(".login").addEventListener("click", () => {
+  console.log(`Login clicked!`);
+});
