1b3ceda7df88f817a5d184857ae1906ec086c159
May Yeung <lightknightdark@gmail.com>
2022-05-13 14:09:32 +0800

    ggg

--- db.sql
+++ db.sql
@@ -2,31 +2,6 @@ create user veper with password 'abc123' superuser;
 
 
 
-CREATE TABLE users(
-    id SERIAL primary key,
-    usernames VARCHAR(255) not null unique,
-    passwords VARCHAR(255) not null,
-    create_at timestamp,
-    updated_at timestamp,
-    is_admin boolean
-);
-
-ALTER TABLE users ADD unique (usernames);
-
-ALTER TABLE users DROP INDEX (passwords);
-
-DELETE FROM users WHERE id = 4;
-
-DROP INDEX passwords on users;
-
-DROP TABLE post;
-
-ALTER TABLE post DROP ROW (bytea);
-
-INSERT INTO users (id,usernames ,passwords ,create_at,updated_at,is_admin) VALUES (1,'mary','1223',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),TRUE);
-INSERT INTO users (id,usernames ,passwords ,create_at,updated_at,is_admin) VALUES (2,'BEN','12243',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),FALSE);
-INSERT INTO users (id,usernames ,passwords ,create_at,updated_at,is_admin) VALUES (3,'peter','12253',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),FALSE);
-=======
 
 //////////////////////////////////////////////////////////
 create database blog;
