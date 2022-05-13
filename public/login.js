c26792df74a1c58777777b61822a6b00b93d3318
Veper <veper.chesley@protonmail.com>
2022-05-13 15:58:02 +0800

    adding loading page at sign up and login

--- public/login.js
+++ public/login.js
@@ -1,56 +1,68 @@
-let loginForm = document.querySelector('#login-form')
+let loginForm = document.querySelector("#login-form");
 //-- ensure all input are filled------
 function validateForm() {
-    let a = document.forms["Form"]["username"].value;
-    let b = document.forms["Form"]["password"].value;
+  let a = document.forms["Form"]["username"].value;
+  let b = document.forms["Form"]["password"].value;
 
-    if (!a || !b) {
-      alert("Please Fill All Required Fields");
-      return false;
-    }
+  if (!a || !b) {
+    alert("Please Fill All Required Fields");
+    return false;
   }
+}
 
-  //--login method----
+//--login method----
 
-loginForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
-  fetch('/login',{
-    method: 'post',
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch("/login", {
+    method: "post",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({username:loginForm.username.value, password:loginForm.password.value})
+    body: JSON.stringify({
+      username: loginForm.username.value,
+      password: loginForm.password.value,
+    }),
   })
-  .then(res => res.json())
-  .then(json => {
-  //   console.log(json);
-  // console.log('hihi');
+    .then((res) => res.json())
+    .then((json) => {
+      //   console.log(json);
+      // console.log('hihi');
 
-    if (json.error) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: toast => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        },
-      })
+      if (json.error) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: 'error',
-        title: 'Failed to auto login: ' + json.error,
-      })
-      // console.error('failed to check role:', json.error)
-      // return
-    }
-    window.location.href = '/'
-  })
-  .catch(error => ({ error: String(error) }))
-})
+        Toast.fire({
+          icon: "error",
+          title: "Failed to auto login: " + json.error,
+        });
+        // console.error('failed to check role:', json.error)
+        // return
+      }
+      loadingPage();
+    })
+    .catch((error) => ({ error: String(error) }));
+});
 
+function loadingPage() {
+  let myWindow = window.open(
+    "/loading_page.html",
+    "myWindow",
+    "width=500, height=500"
+  );
 
-
-
+  setTimeout(() => {
+    myWindow.close();
+    location.href = "/index.html";
+  }, 1500);
+}
--- public/signup.js
+++ public/signup.js
@@ -1,55 +1,66 @@
 function validateForm() {
-    let a = document.forms["Form"]["username"].value;
-    let b = document.forms["Form"]["password"].value;
+  let a = document.forms["Form"]["username"].value;
+  let b = document.forms["Form"]["password"].value;
 
-    if (!a || !b) {
-      alert("Please Fill All Required Fields");
-      return false;
-    }
+  if (!a || !b) {
+    alert("Please Fill All Required Fields");
+    return false;
   }
+}
 
+let signupForm = document.querySelector("#sign-form");
 
-
-
-
-let signupForm = document.querySelector('#sign-form');
-
-
-signupForm.addEventListener('submit', (e)=>{
-  e.preventDefault()
-  fetch('/signup',{
-    method: 'post',
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch("/signup", {
+    method: "post",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({username:signupForm.username.value, password:signupForm.password.value})
+    body: JSON.stringify({
+      username: signupForm.username.value,
+      password: signupForm.password.value,
+    }),
   })
-  .then(res => res.json())
-  .then(json => {
-  //   console.log(json);
-  // console.log('hihi');
+    .then((res) => res.json())
+    .then((json) => {
+      //   console.log(json);
+      // console.log('hihi');
 
-    if (json.error) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: toast => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        },
-      })
+      if (json.error) {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        });
 
-      Toast.fire({
-        icon: 'error',
-        title: 'Failed to auto signup: ' + json.error,
-      })
-      // console.error('failed to check role:', json.error)
-      // return
-    }
-    window.location.href = '/'
-  })
-  .catch(error => ({ error: String(error) }))
-})
+        Toast.fire({
+          icon: "error",
+          title: "Failed to auto signup: " + json.error,
+        });
+        // console.error('failed to check role:', json.error)
+        // return
+      }
+      loadingPage();
+    })
+    .catch((error) => ({ error: String(error) }));
+});
+
+function loadingPage() {
+  let myWindow = window.open(
+    "/loading_page.html",
+    "myWindow",
+    "width=500, height=500"
+  );
+
+  setTimeout(() => {
+    myWindow.close();
+    location.href = "/index.html";
+  }, 1500);
+}
