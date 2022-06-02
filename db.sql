9b03d2b803f6a14b3770f3b8618fcb307d7f655b
Veper <veper.chesley@protonmail.com>
2022-06-02 19:32:39 +0800

    update

--- db.sql
+++ db.sql
@@ -6,6 +6,8 @@ create user veper with password 'abc123' superuser;
 //////////////////////////////////////////////////////////
 create database blog;
 
+create table users (
+  id serial primary key,
   username varchar(255) not null unique,
   password varchar(255) not null,
   created_at timestamp default current_timestamp,
@@ -13,7 +15,7 @@ create database blog;
   is_admin boolean default false
 );
 
-INSERT INTO users (id,username ,password ,created_at,updated_at,is_admin) VALUES (1,'mary','1223',default(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),TRUE);
+INSERT INTO users (id,username ,password ,created_at,updated_at,is_admin) VALUES (1,'mary','1223',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),TRUE);
 INSERT INTO users (id,username ,password ,created_at,updated_at,is_admin) VALUES (2,'BEN','12243',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),FALSE);
 INSERT INTO users (id,username ,password ,created_at,updated_at,is_admin) VALUES (3,'peter','12253',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),FALSE);
 INSERT INTO users (id,username ,password ,created_at,updated_at,is_admin) VALUES (4,'user','1523',(CURRENT_TIMESTAMP),(CURRENT_TIMESTAMP),FALSE);
