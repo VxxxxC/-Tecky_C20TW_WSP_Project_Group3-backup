45e2f7d6f251565eb4f35b1450f976cd8b0b8ae1
Veper <veper.chesley@protonmail.com>
2022-06-03 22:56:43 +0800

    fix content page without image display

--- public/content.js
+++ public/content.js
@@ -24,7 +24,47 @@ async function postContent() {
   let result = await res.json();
   console.log(result);
   let post = result.posts;
-  if (post.image == null){
+  if (post.image == null) {
+    content.innerHTML = `<div class="title-holder">${post.title}</div>
+    <div class="slider">
+        <div  class="item">
+        <div class="content-img" style="color: rgba(128, 128, 128, 0.5);">user have not upload image of this post😢</div>
+        </div>
+ </div>
+      <div class="text">
+      <div class="core">
+      ${post.content}
+      </div>
+      </div>
+      <button class="edit-btn .float-right ">
+      <i class="bi bi-pencil aria-hidden="true"></i>
+      </button>   
+  
+      <div class="author">
+            <div class="sub-header-1">Author</div>
+            <div class="author-name">${post.username}</div>
+            <div class="isolate"></div>
+        </div>
+
+  
+    <div class="media">
+      <ul>
+        <li>
+       <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>   
+        </li>
+        <li>
+      <a href="https://www.instagram.com"><i class="bi bi-instagram"></i></a>    
+        </li>
+        <li>
+          <a href="https://www.youtube.com"><i class="bi bi-youtube"></i></a>
+        </li>
+      </ul>
+    </div>
+    <div class="copyright">
+      <p>All copyright reserved by the author</p>
+    </div>
+       `;
+  } else {
     content.innerHTML = `<div class="title-holder">${post.title}</div>
     <div class="slider">
         <div  class="item">
@@ -64,49 +104,8 @@ async function postContent() {
       <p>All copyright reserved by the author</p>
     </div>
        `;
-  }else{
-    content.innerHTML = `<div class="title-holder">${post.title}</div>
-    <div class="slider">
-        <div  class="item">
-        <img src="${"/img/" + post.image}">
-        </div>
- </div>
-      <div class="text">
-      <div class="core">
-      ${post.content}
-      </div>
-      </div>
-      <button class="edit-btn .float-right ">
-      <i class="bi bi-pencil aria-hidden="true"></i>
-      </button>   
-  
-      <div class="author">
-            <div class="sub-header-1">Author</div>
-            <div class="author-name">${post.username}</div>
-            <div class="isolate"></div>
-        </div>
-
-  
-    <div class="media">
-      <ul>
-        <li>
-       <a href="https://www.facebook.com/"><i class="bi bi-facebook"></i></a>   
-        </li>
-        <li>
-      <a href="https://www.instagram.com"><i class="bi bi-instagram"></i></a>    
-        </li>
-        <li>
-          <a href="https://www.youtube.com"><i class="bi bi-youtube"></i></a>
-        </li>
-      </ul>
-    </div>
-    <div class="copyright">
-      <p>All copyright reserved by the author</p>
-    </div>
-       `;
-
   }
-  
+
   let editBtn = document.querySelector(".edit-btn");
   editBtn.addEventListener("click", () => {
     console.log("edit post");
